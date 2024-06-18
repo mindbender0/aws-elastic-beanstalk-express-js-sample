e9f15a4bb1a47e55466bd801cb7d2e2deaecd9d4
Aland Azad <62204766+mindbender0@users.noreply.github.com>
2024-06-18 15:42:39 +0300

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello From the Other Side!'));
+app.get('/', (req, res) => res.send('Hello this is hosted on AWS!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
