3b1364ab7b8893698fd1b5c6d5bc7a3ad11e19df
Aland Azad <62204766+mindbender0@users.noreply.github.com>
2024-06-18 15:53:55 +0300

    Adding a new route to app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => res.send('Hello this is hosted on AWS!'));
+app.get('/test', (req, res) => res.send('Added this /test new route just for funnnnn!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
